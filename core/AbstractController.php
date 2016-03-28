4109a6ed3cb491668c299c26ec4ccb8ae605ce33
apolo96 <andresfpvclap@gmail.com>
2016-03-28 12:56:32 -0500

    AbstractController.php base terminada, sujeto a cambios

--- core/AbstractController.php
+++ core/AbstractController.php
@@ -1,4 +1,33 @@
 <?php
 abstract  class AbstractController{
-    
+
+
+    abstract function onEventHandler();
+
+    abstract function index();
+
+    protected function helperData(){
+        $datos['post'] = array();
+        if(isset($_POST)){
+            $datos['post'] = $_POST;
+        }
+        return $datos;
+    }
+    protected function helperFile(){
+        $file['file'] = array();
+        if($_FILES){
+            $file['file'] = $_FILES;
+        }
+        return $file;
+    }
+
+    protected function getEvent($eventos,$modulo){
+        $event = "index";
+        $uri = $_SERVER['REQUEST_URI'];
+        foreach ($eventos as $keyEvent => $evento){
+            $uri_event = $modulo.$evento."/";
+            if(strpos($uri,$uri_event) == True) $event = $keyEvent;
+        }
+        return $event;
+    }
 }
